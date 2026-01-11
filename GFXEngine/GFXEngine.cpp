9fa00928be68d0f0d5c232a5859b40d87e2410d5
Andreas Wagner <andy16823@live.de>
2026-01-11 16:35:58 +0100

    Projektdateien hinzufügen.

---
+++ GFXEngine/GFXEngine.cpp
@@ -0,0 +1,12 @@
+﻿// GFXEngine.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "GFXEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
