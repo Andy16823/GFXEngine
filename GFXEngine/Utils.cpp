f6c91fa860330c754d7982c23df07f66d5721f81
Andreas Wagner <andy16823@live.de>
2026-02-17 21:27:38 +0100

    Add stb image loader and GFXEngine utils
    Fetch stb (stb_image) via FetchContent, create an INTERFACE target (stb_image) and expose its include directory for use by other targets. Add GFXEngine/Utils.h and GFXEngine/Utils.cpp implementing a simple loadImage() utility that uses stb_image to load pixel data into LibGFX::ImageData (sets VK_FORMAT_R8G8B8A8_UNORM and throws on failure). Register Utils.{h,cpp} in GFXEngine CMakeLists and link the new stb_image target to the GFXEngine library.
    
    This enables header-only image loading support and integrates it into the GFXEngine build.

---
+++ GFXEngine/Utils.cpp
@@ -0,0 +1,19 @@
+#include "Utils.h"
+#include "stb_image.h"
+#include <algorithm>
+#include <stdexcept>
+
+LibGFX::ImageData GFXEngine::Utils::loadImage(const std::string& filePath)
+{
+	int texWidth, texHeight, texChannels;
+	stbi_uc* pixels = stbi_load(filePath.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+	if (!pixels) {
+		throw std::runtime_error("Failed to load texture image!");
+	}
+	LibGFX::ImageData imageData;
+	imageData.pixels = pixels;
+	imageData.width = static_cast<uint32_t>(texWidth);
+	imageData.height = static_cast<uint32_t>(texHeight);
+	imageData.format = VK_FORMAT_R8G8B8A8_UNORM;
+	return imageData;
+}
---
+++ GFXEngine/Utils.h
@@ -0,0 +1,15 @@
+#pragma once
+#define STB_IMAGE_IMPLEMENTATION
+#include <string>
+#include "DataTypes.h"
+#include "Imaging.h"
+#include "stb_image.h"
+
+// TODO: Move image loading in an own class. For now , this is a simple utility function to load an image from disk and return the pixel data along with the image dimensions and format. In the future, we can expand this to support more formats, error handling, and caching of loaded images if needed.
+namespace GFXEngine {
+	class Utils
+	{
+	public:
+		static LibGFX::ImageData loadImage(const std::string& filePath);
+	};
+}
