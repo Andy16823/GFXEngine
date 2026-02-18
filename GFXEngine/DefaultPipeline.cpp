983d30c6f937de6789cbe7dc1782a65333c05102
Andreas Wagner <andy16823@live.de>
2026-02-18 20:01:28 +0100

    Add push constant range to pipeline layout
    Define a VkPushConstantRange (vertex + fragment stages, offset 0, size = sizeof(glm::mat4)) and attach it to VkPipelineLayoutCreateInfo by setting pushConstantRangeCount and pPushConstantRanges. This enables passing a mat4 via push constants to shaders in DefaultPipeline.cpp.

--- GFXEngine/DefaultPipeline.cpp
+++ GFXEngine/DefaultPipeline.cpp
@@ -136,11 +136,18 @@ void GFXEngine::Graphics::DefaultPipeline::create(VkContext& context)
 	colorBlending.pAttachments = &colorBlendAttachment;
 
 	// Pipeline Layout
+	VkPushConstantRange pushConstantRange = {};	
+	pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+	pushConstantRange.offset = 0;
+	pushConstantRange.size = sizeof(glm::mat4);
+
 	std::array<VkDescriptorSetLayout, 2> layouts = { m_uniformsLayout, m_textureLayout };
 	VkPipelineLayoutCreateInfo pipelineLayoutInfo = {};
 	pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
 	pipelineLayoutInfo.setLayoutCount = static_cast<uint32_t>(layouts.size());
 	pipelineLayoutInfo.pSetLayouts = layouts.data();
+	pipelineLayoutInfo.pushConstantRangeCount = 1;
+	pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
 
 	if (vkCreatePipelineLayout(device, &pipelineLayoutInfo, nullptr, &m_pipelineLayout) != VK_SUCCESS) {
 		throw std::runtime_error("failed to create pipeline layout!");
