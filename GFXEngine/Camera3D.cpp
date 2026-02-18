e0ce75d05bd826c8f8a415d13873713698e77d21
Andreas Wagner <andy16823@live.de>
2026-02-18 20:04:45 +0100

    Compute real view/proj in Camera3D buffer
    Replace placeholder identity matrices with actual view and projection calculations in getCameraBufferObject: create proj via glm::perspective, flip Y for Vulkan, set view using glm::lookAt, and populate cameraPos. Commented-out placeholder code was moved below for reference. This enables proper camera matrices to be provided to shaders.

--- GFXEngine/Camera3D.cpp
+++ GFXEngine/Camera3D.cpp
@@ -16,17 +16,17 @@ GFXEngine::Graphics::Camera3D::Camera3D(glm::vec3 position, float aspect, float 
 GFXEngine::EngineTypes::CameraBufferObject GFXEngine::Graphics::Camera3D::getCameraBufferObject() const
 {
 	// TODO: Implement this later when model view projection matrices are properly set up in the shader. For now, just return an empty buffer object.
-	//glm::mat4 proj = glm::perspective(m_fov, m_aspectRatio, m_nearPlane, m_farPlane);
-	//proj[1][1] *= -1; // Invert Y coordinate for Vulkan
-	//EngineTypes::CameraBufferObject bufferObject;
-	//bufferObject.view = glm::lookAt(m_position, m_target, m_up);
-	//bufferObject.proj = proj;
-	//bufferObject.cameraPos = m_position;
-	//return bufferObject;
-
-	EngineTypes::CameraBufferObject bufferObject{};
-	bufferObject.view = glm::mat4(1.0f);
-	bufferObject.proj = glm::mat4(1.0f);
+	glm::mat4 proj = glm::perspective(m_fov, m_aspectRatio, m_nearPlane, m_farPlane);
+	proj[1][1] *= -1; // Invert Y coordinate for Vulkan
+	EngineTypes::CameraBufferObject bufferObject;
+	bufferObject.view = glm::lookAt(m_position, m_target, m_up);
+	bufferObject.proj = proj;
 	bufferObject.cameraPos = m_position;
 	return bufferObject;
+
+	//EngineTypes::CameraBufferObject bufferObject{};
+	//bufferObject.view = glm::mat4(1.0f);
+	//bufferObject.proj = glm::mat4(1.0f);
+	//bufferObject.cameraPos = m_position;
+	//return bufferObject;
 }
