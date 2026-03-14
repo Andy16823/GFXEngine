0723ec994ae8ae324f721e6e40fdea9ade3ede68
Andreas Wagner <andy16823@live.de>
2026-03-14 17:31:09 +0100

    Make vertex/index buffers host-coherent
    Add VK_MEMORY_PROPERTY_HOST_COHERENT_BIT to vertex and index buffer allocations and stop forcing explicit flushes in renderer.updateBuffer (changed last arg from true to false). This lets CPU writes be immediately visible to the GPU when using host-coherent memory. TODO about switching to staging buffers for devices without rebar support remains.

--- GFXEngine/Mesh.cpp
+++ GFXEngine/Mesh.cpp
@@ -4,12 +4,12 @@ void GFXEngine::Graphics::Mesh::init(Renderer& renderer)
 {
 	// TODO: Currently using rebar buffers for simplicity, but we should eventually switch to staging buffers for fallback support on devices that don't support rebar buffers
 	size_t vertexBufferSize = m_vertices.size() * sizeof(EngineTypes::Vertex3D);
-	m_vertexBuffer = renderer.createBuffer(vertexBufferSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
-	renderer.updateBuffer(m_vertexBuffer, m_vertices.data(), m_vertices.size(), true);
+	m_vertexBuffer = renderer.createBuffer(vertexBufferSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+	renderer.updateBuffer(m_vertexBuffer, m_vertices.data(), m_vertices.size(), false);
 
 	size_t indexBufferSize = m_indices.size() * sizeof(uint32_t);
-	m_indexBuffer = renderer.createBuffer(indexBufferSize, VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
-	renderer.updateBuffer(m_indexBuffer, m_indices.data(), m_indices.size(), true);
+	m_indexBuffer = renderer.createBuffer(indexBufferSize, VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+	renderer.updateBuffer(m_indexBuffer, m_indices.data(), m_indices.size(), false);
 }
 
 void GFXEngine::Graphics::Mesh::draw(Renderer& renderer, uint32_t imageIndex) const
