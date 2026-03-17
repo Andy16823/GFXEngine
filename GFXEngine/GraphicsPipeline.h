88a4123b46674e47ba2e80ce37c0b69f52e72a40
Andreas Wagner <andy16823@live.de>
2026-03-17 18:12:55 +0100

    Delete copy, enable move, fix setters
    Prevent accidental copying of GraphicsPipeline by deleting copy ctor/assignment and enable move semantics (defaulted move ctor/assignment) for safe transfer of Vulkan handles. Fix setter bugs where parameters were assigned to themselves by using member access (this->viewport/this->scissor) and add setRenderPass(VkRenderPass) to allow assigning the render pass. These changes avoid resource-safety issues and correct previously broken setter behavior.

--- GFXEngine/GraphicsPipeline.h
+++ GFXEngine/GraphicsPipeline.h
@@ -12,13 +12,19 @@ namespace GFXEngine {
 		public:
 			GraphicsPipeline(const RenderShader& shader) 
 				: shader(shader), renderPass(VK_NULL_HANDLE), pipeline(VK_NULL_HANDLE), pipelineLayout(VK_NULL_HANDLE), viewport({}), scissor({}) {}
-			~GraphicsPipeline() = default;
+
+			GraphicsPipeline(const GraphicsPipeline&) = delete;
+			GraphicsPipeline& operator=(const GraphicsPipeline&) = delete;
+
+			GraphicsPipeline(GraphicsPipeline&&) = default;
+			GraphicsPipeline& operator=(GraphicsPipeline&&) = default;
 
 			VkPipelineLayout getPipelineLayout() const override { return pipelineLayout; }
 			VkPipeline getPipeline() const override { return pipeline; }
 			
-			void setViewport(VkViewport viewport) { viewport = viewport; }
-			void setScissor(VkRect2D scissor) { scissor = scissor; }
+			void setRenderPass(VkRenderPass renderPass) { this->renderPass = renderPass; }
+			void setViewport(VkViewport viewport) { this->viewport = viewport; }
+			void setScissor(VkRect2D scissor) { this->scissor = scissor; }
 			void setDescriptorSetLayouts(std::span<const VkDescriptorSetLayout> layouts) {
 				descriptorSetLayouts.assign(layouts.begin(), layouts.end());
 			}
