474a5da2464aae545387a340d524bac989d96036
Andreas Wagner <andy16823@live.de>
2026-03-17 22:56:02 +0100

    Remove GFXEngine/Shader sources and SPIR-V
    Delete the GFXEngine/Shader directory contents: shader source files (.vert, .frag), the compile.bat script, and compiled SPIR-V binaries. This removes both the basic and PBR shader implementations and their compiled artifacts from the repository (cleanup or relocation of shader assets).

--- GFXEngine/Shader/compile.bat
+++
@@ -1,6 +0,0 @@
-C:\VulkanSDK\1.4.328.1\Bin\glslangValidator.exe -V shader.vert -o vert.spv
-C:\VulkanSDK\1.4.328.1\Bin\glslangValidator.exe -V shader.frag -o frag.spv
-
-C:\VulkanSDK\1.4.328.1\Bin\glslangValidator.exe -V pbr_shader.vert -o pbr_vert.spv
-C:\VulkanSDK\1.4.328.1\Bin\glslangValidator.exe -V pbr_shader.frag -o pbr_frag.spv
-pause
