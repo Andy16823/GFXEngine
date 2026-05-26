37aeaa6f94f8bbe7cf61a0ff5c332c1cdf28e7d2
Andreas Wagner <andy16823@live.de>
2026-05-26 20:19:51 +0200

    Add MeshBuilder and include in CMake
    Introduce MeshBuilder (MeshBuilder.h/.cpp) as a utility for constructing Mesh objects by accumulating vertices and indices. Implements multiple addVertex overloads, addTriangle/addQuad helpers, and a build() method to produce a GFXEngine::Graphics::Mesh. Also update GFXEngine/CMakeLists.txt to register MeshBuilder.h and MeshBuilder.cpp with the GFXEngine static library.

---
+++ GFXEngine/MeshBuilder.cpp
@@ -0,0 +1,80 @@
+#include "MeshBuilder.h"
+
+using namespace GFXEngine;
+using namespace GFXEngine::Core;
+using namespace GFXEngine::EngineTypes;
+using namespace GFXEngine::Graphics;
+
+void GFXEngine::Core::MeshBuilder::addVertex(const EngineTypes::Vertex3D& vertex)
+{
+	vertices.push_back(vertex);
+}
+
+GFXEngine::Graphics::Mesh MeshBuilder::build() const
+{
+	Mesh mesh;
+	mesh.setVertices(vertices);
+	mesh.setIndices(indices);
+	return mesh;	
+}
+
+void MeshBuilder::addQuad(uint32_t index1, uint32_t index2, uint32_t index3, uint32_t index4)
+{
+	indices.push_back(index1);
+	indices.push_back(index2);
+	indices.push_back(index3);
+	indices.push_back(index1);
+	indices.push_back(index3);
+	indices.push_back(index4);
+}
+
+void MeshBuilder::addTriangle(uint32_t index1, uint32_t index2, uint32_t index3)
+{
+	indices.push_back(index1);
+	indices.push_back(index2);
+	indices.push_back(index3);
+}
+
+void MeshBuilder::addVertex(const glm::vec3& pos, const glm::vec3& color, const glm::vec2& texCoord, const glm::vec3& normal, const glm::vec4& tangent)
+{
+	Vertex3D vertex;
+	vertex.pos = pos;
+	vertex.color = color;
+	vertex.texCoord = texCoord;
+	vertex.normal = normal;
+	vertex.tangent = tangent;
+	vertices.push_back(vertex);
+}
+
+void MeshBuilder::addVertex(const glm::vec3& pos, const glm::vec3& color, const glm::vec2& texCoord, const glm::vec3& normal)
+{
+	Vertex3D vertex;
+	vertex.pos = pos;
+	vertex.color = color;
+	vertex.texCoord = texCoord;
+	vertex.normal = normal;
+	vertex.tangent = glm::vec4(0.0f);
+	vertices.push_back(vertex);
+}
+
+void MeshBuilder::addVertex(const glm::vec3& pos, const glm::vec3& color, const glm::vec2& texCoord)
+{
+	Vertex3D vertex;
+	vertex.pos = pos;
+	vertex.color = color;
+	vertex.texCoord = texCoord;
+	vertex.normal = glm::vec3(0.0f);
+	vertex.tangent = glm::vec4(0.0f);
+	vertices.push_back(vertex);
+}
+
+void MeshBuilder::addVertex(const glm::vec3& pos, const glm::vec3& color)
+{
+	Vertex3D vertex;
+	vertex.pos = pos;
+	vertex.color = color;
+	vertex.texCoord = glm::vec2(0.0f);
+	vertex.normal = glm::vec3(0.0f);
+	vertex.tangent = glm::vec4(0.0f);
+	vertices.push_back(vertex);
+}
---
+++ GFXEngine/MeshBuilder.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "DataTypes.h"
+#include "Mesh.h"
+
+namespace GFXEngine
+{
+	namespace Core
+	{
+		/// <summary>
+		/// MeshBuilder is a utility class for constructing Mesh objects by allowing the user to add vertices and indices incrementally.
+		/// </summary>
+		class MeshBuilder
+		{
+		private:
+			std::vector<EngineTypes::Vertex3D> vertices;
+			std::vector<uint32_t> indices;
+
+		public:
+			void addVertex(const EngineTypes::Vertex3D& vertex);
+			void addVertex(const glm::vec3& pos, const glm::vec3& color);
+			void addVertex(const glm::vec3& pos, const glm::vec3& color, const glm::vec2& texCoord);
+			void addVertex(const glm::vec3& pos, const glm::vec3& color, const glm::vec2& texCoord, const glm::vec3& normal);
+			void addVertex(const glm::vec3& pos, const glm::vec3& color, const glm::vec2& texCoord, const glm::vec3& normal, const glm::vec4& tangent);
+			void addTriangle(uint32_t index1, uint32_t index2, uint32_t index3);
+			void addQuad(uint32_t index1, uint32_t index2, uint32_t index3, uint32_t index4);
+			uint32_t getCurrentVertexIndex() const { return static_cast<uint32_t>(vertices.size()); }
+			GFXEngine::Graphics::Mesh build() const;
+		};
+	}
+}
