2bec2124ac03a5f1e57b49fb4e2e8b1a4a6b59f7
Andreas Wagner <andy16823@live.de>
2026-07-15 07:22:23 +0200

    Use Mesh type in MeshRenderBehavior
    Update MeshRenderBehavior to include `Mesh.h` and use `Graphics::Mesh*` in its member and constructor instead of `Graphics::Mesh3D*`. This aligns the behavior with the newer mesh type and keeps rendering interfaces consistent.

--- GFXEngine/MeshRenderBehavior.h
+++ GFXEngine/MeshRenderBehavior.h
@@ -1,19 +1,19 @@
 #pragma once
 #include "Behavior.h"
 #include "IRenderable.h"
-#include "Mesh3D.h"
+#include "Mesh.h"
 
 namespace GFXEngine {
 	namespace Core {
 		class MeshRenderBehavior : public Behavior, public Graphics::IRenderable
 		{
 		private:
-			Graphics::Mesh3D* m_mesh = nullptr;
+			Graphics::Mesh* m_mesh = nullptr;
 			Graphics::Material* m_material = nullptr;
 			Graphics::GraphicsPipeline* m_pipeline = nullptr;
 
 		public:
-			MeshRenderBehavior(Graphics::Mesh3D* mesh, Graphics::Material* material, Graphics::GraphicsPipeline* pipeline)
+			MeshRenderBehavior(Graphics::Mesh* mesh, Graphics::Material* material, Graphics::GraphicsPipeline* pipeline)
 				: m_mesh(mesh), m_material(material), m_pipeline(pipeline) {}
 
 		public:
