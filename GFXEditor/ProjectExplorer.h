006b55af1929333c7bf623dfbea88fbeba3fff4d
Andreas <andy16823@live.de>
2026-08-16 13:49:11 +0200

    fixed documentation command.

--- GFXEditor/ProjectExplorer.h
+++ GFXEditor/ProjectExplorer.h
@@ -104,8 +104,8 @@ namespace GFXEditor {
 			// Parameter: GFXEditor::WorldEditor & editor
 			// Parameter: GLFWwindow * window
 			// Parameter: int key
-            // Parameter: int action
             // Parameter: int scancode
+            // Parameter: int action
             // Parameter: int mods
 			//************************************
             void handleInput(GFXEditor::WorldEditor& editor, GLFWwindow* window, int key, int scancode, int action, int mods) override;
